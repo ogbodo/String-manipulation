62255c5776d654f7fa8dd22d6750bba7865b7b85
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-04-30 23:56:04 +0100

    Returns true if the string contains vowels

--- customString.js
+++ customString.js
@@ -1,7 +1,16 @@
 function CustomString() {
   //customString().call(this);
 }
+String.prototype.hasVowels = function() {
+  var vowelRegex = /[aeiou]/g;
+  console.log(this);
 
-CustomString.prototype = Object.create(string.prototype);
-CustomString.constructor = CustomString;
+  return vowelRegex.test(this);
+};
 
+CustomString.prototype = Object.create(String.prototype);
+// CustomString.prototype.hasVowels = function() {};
+
+/**Start customizing the prototype of the string class to suite my own implementations */
+
+module.exports = CustomString;
