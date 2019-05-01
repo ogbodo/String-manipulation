4966b3f41f4c0338f0b3faad3db2915924103c95
Izuchukwu Ogbodo <matthiasogbodo@gmail.com>
2019-05-01 16:41:17 +0100

    Return true if the string is a question (ending with a question mark)

--- customString.js
+++ customString.js
@@ -2,10 +2,30 @@ function CustomString() {
   //customString().call(this);
 }
 String.prototype.hasVowels = function() {
-  var vowelRegex = /[aeiou]/g;
   console.log(this);
 
+  var vowelRegex = /[aeiou]/gi;
+
   return vowelRegex.test(this);
+};
+String.prototype.isQuestion = function() {
+  var isQuestionRegex = /\w+[?]/;
+  return isQuestionRegex.test(this);
+};
+String.prototype.toUpper = function() {
+  console.log(this);
+  //   var regex = /(.+)\U\1/g;
+  var regex = /(.+)\U$1/g;
+
+  var matchedText = regex.exec(this);
+  console.log(matchedText);
+
+  var upeerCaseRegex = /\U/.exec(matchedText);
+  //   matchedText.forEach(function(value, index, arr) {
+  //     console.log(value);
+  //     console.log(index);
+  console.log(regex.exec(this));
+  //   });
 };
 
 CustomString.prototype = Object.create(String.prototype);
--- customString.test.js
+++ customString.test.js
@@ -4,3 +4,17 @@ test("Returns true if the string contains vowels.", function() {
   var str = "My name is King Solomon and I am a software engineer";
   expect(str.hasVowels()).toBeTruthy();
 });
+
+test("Returns false if the string does not contain vowels.", function() {
+  var str = "KSlmlnpsq";
+  expect(str.hasVowels()).toBeFalsy();
+});
+test("Return true if the string is a question (ending with a question mark).", function() {
+  var str = "are you going home?";
+  expect(str.hasVowels()).toBeTruthy();
+});
+
+// test("Returns the String in question but with all characters in upper cases as applicable..", function() {
+//   var str = "King Solomon";
+//   expect(str.toUpper()).toBe("");
+// });
